c0188da8a260c65318690b2b099a5583673a0a4a
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 16:14:42 +0000

    fix: 3 - RETURN_NULLABLE - return gameWindow; - returning @Nullable expression from method with @NonNull return type

--- src/de/gurkenlabs/litiengine/Game.java
+++ src/de/gurkenlabs/litiengine/Game.java
@@ -85,7 +85,7 @@ public final class Game {
 
   private static GameLoop gameLoop;
   @Nullable private static ScreenManager screenManager;
-  @Nullable private static GameWindow gameWindow;
+  private static GameWindow gameWindow = new GameWindow();
 
   private static GameWorld world = new GameWorld();
 
@@ -265,6 +265,7 @@ public final class Game {
    * @see GameWindow#setIcon(java.awt.Image)
    * @see GameWindow#cursor()
    */
+  @Nullable
   public static GameWindow window() {
     return gameWindow;
   }
