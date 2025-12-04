a891d799a76b9420b775eff8435c325f30d6cb13
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 16:58:19 +0000

    Revert "fix: 31 - DEREFERENCE_NULLABLE - return Collections.unmodifiableCollection(this.miscEntities.get(renderType).values()); - dereferenced expression this.miscEntities.get(renderType) is @Nullable"
    This reverts commit 73827db79b3eda88614be13ab5e9cf9d03a6bb40.

--- src/de/gurkenlabs/litiengine/environment/Environment.java
+++ src/de/gurkenlabs/litiengine/environment/Environment.java
@@ -1100,12 +1100,7 @@ public final class Environment implements IRenderable {
    * @see ILayer#getRenderType()
    */
   public Collection<IEntity> getEntities(final RenderType renderType) {
-    Map<Integer, IEntity> entitiesByType = this.miscEntities.get(renderType);
-    if (entitiesByType == null) {
-      return Collections.emptySet();
-    }
-
-    return Collections.unmodifiableCollection(entitiesByType.values());
+    return Collections.unmodifiableCollection(this.miscEntities.get(renderType).values());
   }
 
   /**
