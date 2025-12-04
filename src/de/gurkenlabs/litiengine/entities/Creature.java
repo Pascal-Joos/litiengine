b44826647adadafd4aa68e1d2f8a2d9037a1f771
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 16:20:51 +0000

    fix: 8 - RETURN_NULLABLE - return this.velocity; - returning @Nullable expression from method with @NonNull return type

--- src/de/gurkenlabs/litiengine/entities/Creature.java
+++ src/de/gurkenlabs/litiengine/entities/Creature.java
@@ -35,7 +35,6 @@ public class Creature extends CombatEntity implements IMobileEntity {
   @TmxProperty(name = MapObjectProperty.MOVEMENT_TURNONMOVE)
   private boolean turnOnMove;
 
-  @Nullable
   @TmxProperty(name = MapObjectProperty.MOVEMENT_VELOCITY)
   private Attribute<Float> velocity;
 
@@ -68,6 +67,8 @@ public class Creature extends CombatEntity implements IMobileEntity {
       this.deceleration = movementInfo.deceleration();
       this.setTurnOnMove(movementInfo.turnOnMove());
       this.addController(this.createMovementController());
+    } else {
+      this.velocity = new Attribute<>(0f);
     }
 
     if (spritesheetName != null) {
