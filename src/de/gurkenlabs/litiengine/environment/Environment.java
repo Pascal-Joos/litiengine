3ad13be1850d98d10a24ae47f643cf5ad6992f5b
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 16:54:47 +0000

    fix: 28 - DEREFERENCE_NULLABLE - this.renderListeners.get(renderType).clear(); - dereferenced expression this.renderListeners.get(renderType) is @Nullable

--- src/de/gurkenlabs/litiengine/environment/Environment.java
+++ src/de/gurkenlabs/litiengine/environment/Environment.java
@@ -455,7 +455,11 @@ public final class Environment implements IRenderable {
 
     for (RenderType renderType : RenderType.values()) {
       this.miscEntities.get(renderType).clear();
-      this.renderListeners.get(renderType).clear();
+      Collection<EnvironmentRenderedListener> listenersForType =
+          this.renderListeners.get(renderType);
+      if (listenersForType != null) {
+        listenersForType.clear();
+      }
       this.renderables.get(renderType).clear();
     }
 
