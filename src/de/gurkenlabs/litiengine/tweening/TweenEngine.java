d214a76ca5e344dd5ae533ad927d9795883a079e
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 10:41:48 +0000

    Revert "fix: 144 - DEREFERENCE_NULLABLE - + tween.getEquation().compute(elapsed / (float) tween.getDuration()) - dereferenced expression tween.getEquation() is @Nullable"
    This reverts commit bbdb64d6972db19917c75051749430ae94f38046.

--- src/de/gurkenlabs/litiengine/tweening/TweenEngine.java
+++ src/de/gurkenlabs/litiengine/tweening/TweenEngine.java
@@ -1,65 +1,140 @@
 package de.gurkenlabs.litiengine.tweening;
 
 import de.gurkenlabs.litiengine.Game;
-import java.util.HashMap;
+import de.gurkenlabs.litiengine.ILaunchable;
+import de.gurkenlabs.litiengine.IUpdateable;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.annotation.Nullable;
 
-public class TweenEngine implements IUpdateable {
-
+/**
+ * The TweenEngine is the central manager for Tweens. It tracks all current Tween instances and
+ * applies their {@code TweenEquation} with every {@code GameLoop} update.
+ */
+public class TweenEngine implements IUpdateable, ILaunchable {
   private final Map<Tweenable, Map<TweenType, Tween>> tweens;
 
+  /** Instantiates a new {@code TweenEngine}. */
   public TweenEngine() {
-    this.tweens = new HashMap<>();
+    this.tweens = new ConcurrentHashMap<>();
   }
 
+  /**
+   * Begins a new Tween. If a Tween is already registered for the {@code Tweenable} with the given
+   * {@code TweenType}, it is restarted with the given duration.
+   *
+   * @param target the {@code Tweenable} target object
+   * @param type the {@code TweenType} determining which values of the target object will be
+   *     modified.
+   * @param duration the duration of the Tween in milliseconds.
+   * @return the Tween instance
+   */
+  public Tween begin(final Tweenable target, final TweenType type, final int duration) {
+    Tween tween = this.getTween(target, type);
+    if (tween == null) {
+      tween = new Tween(target, type, duration).ease(TweenFunction.QUAD_INOUT);
+      this.getTweens().get(target).put(type, tween);
+    } else {
+      tween.setDuration(duration);
+    }
+    tween.begin();
+    return tween;
+  }
+
+  /**
+   * Attempts to get a previously registered {@code Tween} or registers a new one.
+   *
+   * @param target the {@code Tweenable} target object
+   * @param type the {@code TweenType} determining which values of the target object will be
+   *     modified.
+   * @return the Tween instance
+   */
+  @Nullable
+  public Tween getTween(final Tweenable target, final TweenType type) {
+    if (this.getTweens().get(target) == null) {
+      this.getTweens().put(target, new ConcurrentHashMap<>());
+    }
+
+    return this.getTweens().get(target).get(type);
+  }
+
+  /**
+   * Gets the map of registered {@code Tweens}.
+   *
+   * @return the map of registered {@code Tweens}.
+   */
   public Map<Tweenable, Map<TweenType, Tween>> getTweens() {
     return this.tweens;
   }
 
-  public Tween tween(
-      final Tweenable target,
-      final TweenType type,
-      final int duration,
-      final float... targetValues) {
-    return this.tween(target, type, duration, null, targetValues);
-  }
-
-  public Tween tween(
-      final Tweenable target,
-      final TweenType type,
-      final int duration,
-      final TweenFunction easingFunction,
-      final float... targetValues) {
-    if (target == null || type == null) {
-      return null;
+  /**
+   * Looks for a registered Tween instance with the given target and type. Attempts to stop the
+   * Tween and reset the {@code Tweenable} values to the start values.
+   *
+   * @param target the {@code Tweenable} target object
+   * @param type the {@code TweenType} determining which values of the target object will be
+   *     modified.
+   * @return the Tween instance
+   */
+  @Nullable
+  public Tween reset(final Tweenable target, final TweenType type) {
+    final Tween tween = this.getTween(target, type);
+    if (tween != null) {
+      tween.stop();
+      tween.reset();
     }
-
-    final Tween tween = new Tween(target, type, duration);
-    if (easingFunction != null) {
-      tween.ease(easingFunction);
-    }
-
-    tween.target(targetValues).begin();
-
-    this.tweens.computeIfAbsent(target, t -> new HashMap<>()).put(type, tween);
-
-    Game.loop().attach(this);
     return tween;
   }
 
-  public Tween getTween(final Tweenable target, final TweenType type) {
-    if (target == null || type == null) {
-      return null;
+  /**
+   * Looks for a registered Tween instance with the given target and type. Attempts to resume the
+   * Tween if it was stopped.
+   *
+   * @param target the {@code Tweenable} target object
+   * @param type the {@code TweenType} determining which values of the target object will be
+   *     modified.
+   * @return the Tween instance
+   */
+  @Nullable
+  public Tween resume(final Tweenable target, final TweenType type) {
+    final Tween tween = this.getTween(target, type);
+    if (tween != null) {
+      tween.resume();
     }
-
-    final Map<TweenType, Tween> targetTweens = this.tweens.get(target);
-    if (targetTweens == null) {
-      return null;
-    }
-
-    return targetTweens.get(type);
+    return tween;
   }
 
+  /** Start. */
+  @Override
+  public void start() {
+    Game.loop().attach(this);
+  }
+
+  /**
+   * Looks for a registered Tween instance with the given target and type. Attempts to remove the
+   * Tween from the TweenEngine.
+   *
+   * @param target the {@code Tweenable} target object
+   * @param type the {@code TweenType} determining which values of the target object will be
+   *     modified.
+   */
+  public void remove(final Tweenable target, final TweenType type) {
+    final Tween tween = this.getTween(target, type);
+    if (tween != null) {
+      this.getTweens().get(target).remove(type);
+    }
+  }
+
+  /**
+   * Looks for a registered Tween instance with the given target and type. Attempts to stop the
+   * Tween.
+   *
+   * @param target the {@code Tweenable} target object
+   * @param type the {@code TweenType} determining which values of the target object will be
+   *     modified.
+   * @return the Tween instance
+   */
+  @Nullable
   public Tween stop(final Tweenable target, final TweenType type) {
     final Tween tween = this.getTween(target, type);
     if (tween != null) {
@@ -88,22 +163,11 @@ public class TweenEngine implements IUpdateable {
           continue;
         }
         final float[] currentValues = new float[tween.getTargetValues().length];
-        final TweenEquation equation = tween.getEquation();
-        if (equation == null) {
-          // No easing equation specified; fall back to linear interpolation.
-          final float progress = elapsed / (float) tween.getDuration();
-          for (int i = 0; i < tween.getTargetValues().length; i++) {
-            currentValues[i] =
-                tween.getStartValues()[i]
-                    + (tween.getTargetValues()[i] - tween.getStartValues()[i]) * progress;
-          }
-        } else {
-          final float eased = equation.compute(elapsed / (float) tween.getDuration());
-          for (int i = 0; i < tween.getTargetValues().length; i++) {
-            currentValues[i] =
-                tween.getStartValues()[i]
-                    + eased * (tween.getTargetValues()[i] - tween.getStartValues()[i]);
-          }
+        for (int i = 0; i < tween.getTargetValues().length; i++) {
+          currentValues[i] =
+              tween.getStartValues()[i]
+                  + tween.getEquation().compute(elapsed / (float) tween.getDuration())
+                      * (tween.getTargetValues()[i] - tween.getStartValues()[i]);
         }
         tween.getTarget().setTweenValues(tween.getType(), currentValues);
       }
