5763824d3e9699cf0b1e0083a8d9d3d90e89bfa2
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 09:25:18 +0000

    Revert "fix: 86 - DEREFERENCE_NULLABLE - || Game.time().since(this.lastFrameUpdate) < this.getCurrentKeyFrame().getDuration()) { - dereferenced expression this.getCurrentKeyFrame() is @Nullable"
    This reverts commit b3397498ea598583d05532b1c5293775758dc4dd.

--- src/de/gurkenlabs/litiengine/graphics/animation/Animation.java
+++ src/de/gurkenlabs/litiengine/graphics/animation/Animation.java
@@ -315,10 +315,8 @@ public class Animation implements IUpdateable, ILaunchable {
   @Override
   public void update() {
     // do nothing if the animation is not playing or the current keyframe is not finished
-    final KeyFrame currentKeyFrame = this.getCurrentKeyFrame();
     if (!this.isPlaying()
-        || currentKeyFrame == null
-        || Game.time().since(this.lastFrameUpdate) < currentKeyFrame.getDuration()) {
+        || Game.time().since(this.lastFrameUpdate) < this.getCurrentKeyFrame().getDuration()) {
       return;
     }
 
