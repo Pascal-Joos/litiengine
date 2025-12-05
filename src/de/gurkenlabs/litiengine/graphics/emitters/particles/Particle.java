4a4468145bb13ea0681add97ef947e667c8381b8
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 09:31:42 +0000

    Revert "fix: 90 - RETURN_NULLABLE - return this.color; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 63f65a122faa86d85eb97be42e788342c9716a0e.

--- src/de/gurkenlabs/litiengine/graphics/emitters/particles/Particle.java
+++ src/de/gurkenlabs/litiengine/graphics/emitters/particles/Particle.java
@@ -22,7 +22,7 @@ public abstract class Particle implements ITimeToLive {
   private float deltaAngle;
 
   private Collision collisionType;
-  private Color color;
+  @Nullable private Color color;
   private float deltaHeight;
   private float deltaWidth;
 
@@ -74,7 +74,6 @@ public abstract class Particle implements ITimeToLive {
     this.setWidth(width);
     this.setHeight(height);
     this.collisionType = Collision.NONE;
-    this.color = Color.WHITE;
     this.fade = true;
     this.setStopOnCollision(true);
     this.setContinuousCollision(false);
