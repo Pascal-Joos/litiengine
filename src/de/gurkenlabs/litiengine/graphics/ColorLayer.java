4ac483382e2d62fe818ea472a5188f5bb5c76267
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 09:07:33 +0000

    fix: 75 - DEREFERENCE_NULLABLE - final Graphics2D g = this.layer.createGraphics(); - dereferenced expression this.layer is @Nullable

--- src/de/gurkenlabs/litiengine/graphics/ColorLayer.java
+++ src/de/gurkenlabs/litiengine/graphics/ColorLayer.java
@@ -53,6 +53,9 @@ public abstract class ColorLayer implements IRenderable {
   }
 
   public void updateSection(Rectangle2D section) {
+    if (this.layer == null) {
+      return;
+    }
     if (this.getColor() == null) {
       return;
     }
