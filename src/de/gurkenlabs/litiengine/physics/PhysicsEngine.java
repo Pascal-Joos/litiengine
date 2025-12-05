ddac74e1a2112c7ed0b72def6c95eb4cdc268e6b
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 10:11:08 +0000

    Revert "fix: 120 - DEREFERENCE_NULLABLE - this.collisionBoxes.get(type).clear(); - dereferenced expression this.collisionBoxes.get(type) is @Nullable"
    This reverts commit 3d4dc2df0b464a81362993716e5fc1d91129d6ee.

--- src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
+++ src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
@@ -119,10 +119,7 @@ public final class PhysicsEngine implements IUpdateable {
       }
 
       this.collisionEntities.get(type).clear();
-      final java.util.List<java.awt.geom.Rectangle2D> boxesForType = this.collisionBoxes.get(type);
-      if (boxesForType != null) {
-        boxesForType.clear();
-      }
+      this.collisionBoxes.get(type).clear();
     }
 
     this.setBounds(null);
