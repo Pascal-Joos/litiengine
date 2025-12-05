6c1838672245d7edc2404f3ef2cc89840d312e92
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 10:05:19 +0000

    Revert "fix: 115 - RETURN_NULLABLE - return keyboard; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 36b96c65e661146a1a435d5bc08fad1171d3d65d.

--- src/de/gurkenlabs/litiengine/input/Input.java
+++ src/de/gurkenlabs/litiengine/input/Input.java
@@ -20,7 +20,7 @@ public final class Input {
   private static final Logger log = Logger.getLogger(Input.class.getName());
 
   @Nullable private static GamepadManager gamePadManager;
-  private static IKeyboard keyboard = new Keyboard();
+  @Nullable private static IKeyboard keyboard;
   @Nullable private static IMouse mouse;
 
   private Input() {
