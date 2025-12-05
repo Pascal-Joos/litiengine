32e5a4b45d42df9edd7e61964b06f3894f51aa73
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 08:38:23 +0000

    Revert "fix: 53 - RETURN_NULLABLE - return this.layer; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 6ccfd69aa3d78ca4fc169e0b7d44a80321ec359f.

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/MapObject.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/MapObject.java
@@ -366,9 +366,6 @@ public class MapObject extends CustomPropertyProvider implements IMapObject {
 
   @Override
   public IMapObjectLayer getLayer() {
-    if (this.layer == null) {
-      throw new IllegalStateException("MapObject layer has not been set");
-    }
     return this.layer;
   }
 
