080a2c81893a8c14d24a4c47dff2841cea6b4873
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 09:55:55 +0000

    Revert "fix: 107 - METHOD_NO_INIT - protected Slider( - initializer method does not guarantee @NonNull fields button1 (line 12), button2 (line 13) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit d5da361d384060c812c97d3c2f5a4474c84a00cd.

--- src/de/gurkenlabs/litiengine/gui/Slider.java
+++ src/de/gurkenlabs/litiengine/gui/Slider.java
@@ -35,10 +35,6 @@ public abstract class Slider extends GuiComponent {
     this.minValue = minValue;
     this.maxValue = maxValue;
     this.stepSize = stepSize;
-    // initialize default components to satisfy @NonNull guarantees
-    this.button1 = new ImageComponent(x, y, 0, 0);
-    this.button2 = new ImageComponent(x, y, 0, 0);
-    this.sliderComponent = new ImageComponent(x, y, 0, 0);
     this.onChange(e -> this.sliderComponent.setLocation(this.getRelativeSliderPosition()));
   }
 
