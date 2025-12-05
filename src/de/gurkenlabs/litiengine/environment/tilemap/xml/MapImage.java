aefa6090417a65e3a8f9edfa44f116a90464a589
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 08:36:53 +0000

    Revert "fix: 52 - METHOD_NO_INIT - public MapImage(MapImage original) { - initializer method does not guarantee @NonNull fields transparentcolor (line 20), absolutePath (line 28) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit d730b72f28402f671e550cd7d097c0bd25db346b.

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/MapImage.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/MapImage.java
@@ -30,12 +30,6 @@ public class MapImage extends CustomPropertyProvider implements IMapImage {
   /** Instantiates a new {@code MapImage} instance. */
   public MapImage() {
     super();
-    this.transparentcolor = new Color(0, 0, 0, 0);
-    try {
-      this.absolutePath = new URL("file", "", "");
-    } catch (MalformedURLException e) {
-      throw new RuntimeException(e);
-    }
   }
 
   /**
@@ -44,7 +38,7 @@ public class MapImage extends CustomPropertyProvider implements IMapImage {
    * @param original the original we want to copy
    */
   public MapImage(MapImage original) {
-    this();
+    super(original);
 
     if (original == null) {
       return;
