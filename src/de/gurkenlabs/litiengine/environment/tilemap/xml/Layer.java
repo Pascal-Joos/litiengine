f1d91e39647a79bfc13fb7244850579011849a00
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 08:34:43 +0000

    Revert "fix: 50 - RETURN_NULLABLE - return this.parentMap; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit ae000aea2188777c371acc653c2154b1f12e0895.

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/Layer.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/Layer.java
@@ -155,10 +155,6 @@ public abstract class Layer extends CustomPropertyProvider implements ILayer {
 
   @Override
   public IMap getMap() {
-    if (this.parentMap == null) {
-      this.parentMap = new TmxMap();
-    }
-
     return this.parentMap;
   }
 
