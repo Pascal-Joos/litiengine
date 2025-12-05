75cfbae01f7250909a38f4886a71e16dc872e10d
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 10:24:06 +0000

    Revert "fix: 129 - DEREFERENCE_NULLABLE - layer.addMapObject(mapObject); - dereferenced expression layer is @Nullable"
    This reverts commit 4944bcd18db0353fc7a17f75fc21a8237ca2e546.

--- src/de/gurkenlabs/litiengine/resources/Maps.java
+++ src/de/gurkenlabs/litiengine/resources/Maps.java
@@ -266,9 +266,6 @@ public final class Maps extends ResourcesContainer<IMap> {
      * @return The added map object.
      */
     public IMapObject add(@Nullable IMapObjectLayer layer, IMapObject mapObject) {
-      if (layer == null) {
-        throw new IllegalArgumentException("layer must not be null");
-      }
       layer.addMapObject(mapObject);
       return mapObject;
     }
