8cccb6da7d30089135d9c92adba813ca59a4512c
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 15:59:19 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression Resources.spritesheets().get(this.getName()) is @Nullable' /home/vscode/nullness-benchmarks/litiengine/src/de/gurkenlabs/litiengine/graphics/Spritesheet.java:211         && Resources.spritesheets().get(this.getName()).equals(this);

--- src/de/gurkenlabs/litiengine/graphics/Spritesheet.java
+++ src/de/gurkenlabs/litiengine/graphics/Spritesheet.java
@@ -208,7 +208,7 @@ public final class Spritesheet implements Comparable<Spritesheet> {
 
   public boolean isLoaded() {
     return Resources.spritesheets().contains(this.getName())
-        && Resources.spritesheets().get(this.getName()).equals(this);
+        && java.util.Objects.equals(Resources.spritesheets().get(this.getName()), this);
   }
 
   public void setSpriteHeight(final int spriteHeight) {
