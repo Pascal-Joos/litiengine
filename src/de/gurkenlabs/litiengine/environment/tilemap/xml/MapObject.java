774461323c4deb46bebb3bfe5ab3632e4b97a118
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 15:18:04 +0000

    fix: Type='WRONG_OVERRIDE_RETURN', message='method returns @Nullable, but superclass method de.gurkenlabs.litiengine.environment.tilemap.IMapObject.getPolygon() returns @NonNull' /home/vscode/nullness-benchmarks/litiengine/src/de/gurkenlabs/litiengine/environment/tilemap/xml/MapObject.java:210   @Nullable

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/MapObject.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/MapObject.java
@@ -207,9 +207,12 @@ public class MapObject extends CustomPropertyProvider implements IMapObject {
     return this.polyline;
   }
 
-  @Nullable
   @Override
   public IPolyShape getPolygon() {
+    if (this.polygon == null) {
+      this.polygon = new PolyShape();
+    }
+
     return this.polygon;
   }
 
