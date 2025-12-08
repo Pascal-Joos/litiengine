2493e557aa12d72bbdf28f5c6333a41f16c2511a
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 15:15:55 +0000

    fix: Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/vscode/nullness-benchmarks/litiengine/src/de/gurkenlabs/litiengine/environment/tilemap/xml/MapObject.java:369     return this.layer;

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/MapObject.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/MapObject.java
@@ -366,6 +366,9 @@ public class MapObject extends CustomPropertyProvider implements IMapObject {
 
   @Override
   public IMapObjectLayer getLayer() {
+    if (this.layer == null) {
+      throw new IllegalStateException("MapObject layer is not set");
+    }
     return this.layer;
   }
 
