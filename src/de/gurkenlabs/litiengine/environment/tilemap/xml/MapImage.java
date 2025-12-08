65ee4aaf7a4e9dc27d97f34f22761cdc0c177a48
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 15:13:55 +0000

    fix: Type='METHOD_NO_INIT', message='initializer method does not guarantee @NonNull fields transparentcolor (line 20), absolutePath (line 28) are initialized along all control-flow paths (remember to check for exceptions or early returns).' /home/vscode/nullness-benchmarks/litiengine/src/de/gurkenlabs/litiengine/environment/tilemap/xml/MapImage.java:31   public MapImage() {

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/MapImage.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/MapImage.java
@@ -30,6 +30,12 @@ public class MapImage extends CustomPropertyProvider implements IMapImage {
   /** Instantiates a new {@code MapImage} instance. */
   public MapImage() {
     super();
+    this.transparentcolor = new Color(0, 0, 0, 0);
+    try {
+      this.absolutePath = new URL("file:");
+    } catch (MalformedURLException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   /**
