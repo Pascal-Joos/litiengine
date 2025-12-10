e5ff3781ec49ad73ad5c8a9926890a4c500f284c
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 11:52:10 +0000

    fix: 88 - DEREFERENCE_NULLABLE - if (this.data().getTexts().isEmpty()) { - dereferenced expression this.data().getTexts() is @Nullable

--- src/de/gurkenlabs/litiengine/graphics/emitters/Emitter.java
+++ src/de/gurkenlabs/litiengine/graphics/emitters/Emitter.java
@@ -371,8 +371,8 @@ public class Emitter extends Entity implements IUpdateable, ITimeToLive, IRender
         break;
       case TEXT:
         String text;
-        if (this.data().getTexts().isEmpty()) {
-          text = EmitterData.DEFAULT_TEXT;
+        if (this.data().getTexts() == null || this.data().getTexts().isEmpty()) {
+          return null;
         } else {
           text = Game.random().choose(this.data().getTexts());
         }
