6045b96768be1d6685846a300d07b07ccdbc5cf6
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 11:58:56 +0000

    Revert "fix: 94 - DEREFERENCE_NULLABLE - final String name = emitterXml.getFile(); - dereferenced expression emitterXml is @Nullable"
    This reverts commit e743e4d2aaabd923e756a0c33ae577945e39e1d2.

--- src/de/gurkenlabs/litiengine/graphics/emitters/xml/EmitterLoader.java
+++ src/de/gurkenlabs/litiengine/graphics/emitters/xml/EmitterLoader.java
@@ -31,9 +31,6 @@ public class EmitterLoader {
 
   @Nullable
   public static EmitterData load(@Nullable URL emitterXml) {
-    if (emitterXml == null) {
-      return null;
-    }
     final String name = emitterXml.getFile();
     if (loadedEmitters.containsKey(name)) {
       return loadedEmitters.get(name);
