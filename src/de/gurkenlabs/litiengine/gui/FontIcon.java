fbbaec0780bea1dd2ebe8c92f57d1c5ea655db6e
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 12:00:20 +0000

    Revert "fix: 95 - RETURN_NULLABLE - return this.font; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit c9196d38526847df4072a407e61d208a1daee9a1.

--- src/de/gurkenlabs/litiengine/gui/FontIcon.java
+++ src/de/gurkenlabs/litiengine/gui/FontIcon.java
@@ -35,7 +35,6 @@ public class FontIcon {
    *
    * @return the font
    */
-  @Nullable
   public Font getFont() {
     return this.font;
   }
@@ -69,13 +68,10 @@ public class FontIcon {
     final Color oldColor = g.getColor();
     final Font oldFont = g.getFont();
     g.setColor(color);
-    final Font font = this.getFont();
-    if (font != null) {
-      if (bold) {
-        g.setFont(font.deriveFont(Font.BOLD, fontSize));
-      } else {
-        g.setFont(font.deriveFont(fontSize));
-      }
+    if (bold) {
+      g.setFont(this.getFont().deriveFont(Font.BOLD, fontSize));
+    } else {
+      g.setFont(this.getFont().deriveFont(fontSize));
     }
     TextRenderer.render(g, this.getText(), x, y);
     g.setColor(oldColor);
