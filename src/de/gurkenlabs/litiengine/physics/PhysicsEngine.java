618b67ff180421d294e293b8b096678cd5f3f6e6
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 12:45:16 +0000

    fix: 124 - DEREFERENCE_NULLABLE - for (final ICollisionEntity collisionEntity : this.collisionEntities.get(collision)) { - enhanced-for expression this.collisionEntities.get(collision) is @Nullable

--- src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
+++ src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
@@ -541,6 +541,9 @@ public final class PhysicsEngine implements IUpdateable {
   @Nullable
   public RaycastHit raycast(Line2D line, Collision collision, @Nullable ICollisionEntity entity) {
     final Point2D rayCastSource = new Point2D.Double(line.getX1(), line.getY1());
+    if (this.collisionEntities.get(collision) == null) {
+      return null;
+    }
 
     for (final ICollisionEntity collisionEntity : this.collisionEntities.get(collision)) {
       if (!canCollide(entity, collisionEntity)) {
