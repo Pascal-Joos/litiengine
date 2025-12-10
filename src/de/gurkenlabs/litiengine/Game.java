cb1051c72c11e051fbcc563a3471f461b9040f35
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 09:30:26 +0000

    fix: 2 - ASSIGN_FIELD_NULLABLE - gameLoop = null; - assigning @Nullable expression to @NonNull field

--- src/de/gurkenlabs/litiengine/Game.java
+++ src/de/gurkenlabs/litiengine/Game.java
@@ -30,6 +30,7 @@ import de.gurkenlabs.litiengine.sound.SoundPlayback;
 import de.gurkenlabs.litiengine.tweening.TweenEngine;
 import de.gurkenlabs.litiengine.util.ArrayUtilities;
 import de.gurkenlabs.litiengine.util.io.XmlUtilities;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.awt.event.KeyEvent;
 import java.lang.Thread.UncaughtExceptionHandler;
 import java.net.URL;
@@ -83,7 +84,7 @@ public final class Game {
   @Nullable private static GameInfo gameInfo = new GameInfo();
   private static final TweenEngine tweenEngine = new TweenEngine();
 
-  private static GameLoop gameLoop;
+  @Nullable private static GameLoop gameLoop;
   @Nullable private static ScreenManager screenManager;
   @Nullable private static GameWindow gameWindow;
 
@@ -344,6 +345,7 @@ public final class Game {
    * @see ILoop#attach(IUpdateable)
    * @see ILoop#detach(IUpdateable)
    */
+  @Nullable
   public static IGameLoop loop() {
     return gameLoop;
   }
@@ -509,9 +511,11 @@ public final class Game {
    *
    * @param uncaughtExceptionHandler The handler to be used for uncaught exceptions.
    */
-  public static void setUncaughtExceptionHandler(
+  public static synchronized void setUncaughtExceptionHandler(
       UncaughtExceptionHandler uncaughtExceptionHandler) {
-    gameLoop.setUncaughtExceptionHandler(uncaughtExceptionHandler);
+    if (gameLoop != null) {
+      Nullability.castToNonnull(gameLoop).setUncaughtExceptionHandler(uncaughtExceptionHandler);
+    }
     Thread.setDefaultUncaughtExceptionHandler(uncaughtExceptionHandler);
   }
 
@@ -538,7 +542,11 @@ public final class Game {
           "The game cannot be started without being first initialized. Call Game.init(...) before Game.start().");
     }
 
-    gameLoop.start();
+    if (gameLoop == null) {
+      throw new IllegalStateException("Game loop has not been initialized");
+    }
+
+    Nullability.castToNonnull(gameLoop).start();
     tweenEngine.start();
     soundEngine.start();
 
@@ -620,7 +628,9 @@ public final class Game {
     initialized = false;
 
     config().save();
-    gameLoop.terminate();
+    if (gameLoop != null) {
+      gameLoop.terminate();
+    }
     tweenEngine.terminate();
     soundEngine.terminate();
 
