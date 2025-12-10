96dbb4b48e4d9daa2bc146078c40e6c8fba1b9fa
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 10:59:42 +0000

    fix: 42 - DEREFERENCE_NULLABLE - return mapObject.getPolyline().getAbsolutePoints(mapObject.getLocation()); - dereferenced expression mapObject.getPolyline() is @Nullable

--- src/de/gurkenlabs/litiengine/environment/tilemap/MapUtilities.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/MapUtilities.java
@@ -269,7 +269,7 @@ public final class MapUtilities {
       return mapObject.getPolygon().getAbsolutePoints(mapObject.getLocation());
     }
 
-    if (mapObject.isPolyline()) {
+    if (mapObject.isPolyline() && mapObject.getPolyline() != null) {
       return mapObject.getPolyline().getAbsolutePoints(mapObject.getLocation());
     }
 
