5a2eeebea9719f62990322615b161d88969d6512
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 09:49:58 +0000

    fix: 6 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type

--- src/de/gurkenlabs/litiengine/entities/Creature.java
+++ src/de/gurkenlabs/litiengine/entities/Creature.java
@@ -14,6 +14,7 @@ import de.gurkenlabs.litiengine.graphics.animation.IEntityAnimationController;
 import de.gurkenlabs.litiengine.physics.IMovementController;
 import de.gurkenlabs.litiengine.physics.MovementController;
 import de.gurkenlabs.litiengine.tweening.TweenType;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.awt.geom.Point2D;
 import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
@@ -151,7 +152,7 @@ public class Creature extends CombatEntity implements IMobileEntity {
     // ensure that entities don't travel too far in case of lag
     return Math.min(Game.loop().getDeltaTime(), GameLoop.TICK_DELTATIME_LAG)
         * 0.001F
-        * this.getVelocity().get()
+        * Nullability.castToNonnull(this.getVelocity().get())
         * Game.loop().getTimeScale();
   }
 
