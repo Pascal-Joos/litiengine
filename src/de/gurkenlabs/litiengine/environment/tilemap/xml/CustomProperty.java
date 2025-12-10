fb490faae47faa82a99176f4c1f549cf6f1f0f39
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 11:00:25 +0000

    fix: 43 - ASSIGN_FIELD_NULLABLE - this.value = ColorHelper.encode(Objects.requireNonNull(value)); - assigning @Nullable expression to @NonNull field

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/CustomProperty.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/CustomProperty.java
@@ -10,7 +10,7 @@ import javax.annotation.Nullable;
 public class CustomProperty implements ICustomProperty {
 
   private String type;
-  private String value;
+  @Nullable private String value;
   @Nullable private URL location;
 
   /**
@@ -114,6 +114,7 @@ public class CustomProperty implements ICustomProperty {
     this.location = null;
   }
 
+  @Nullable
   @Override
   public String getAsString() {
     return this.value;
@@ -121,8 +122,9 @@ public class CustomProperty implements ICustomProperty {
 
   @Override
   public char getAsChar() {
-    return this.value.charAt(
-        0); // TODO Is this enough? Should it check if it's the right length and throw an exception
+    return Objects.requireNonNull(this.value)
+        .charAt(0); // TODO Is this enough? Should it check if it's the right length and throw an
+    // exception
     // if it's not?
   }
 
