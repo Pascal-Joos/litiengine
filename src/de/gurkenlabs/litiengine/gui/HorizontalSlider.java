df6e747a1ee7e588cfb9b85d74d26d4aa2ebe8f0
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 12:17:19 +0000

    Revert "fix: 107 - METHOD_NO_INIT - protected Slider( - initializer method does not guarantee @NonNull fields button1 (line 12), button2 (line 13) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 801b077918ffa93e41bb90b63a6cf6fcbd672b3f.

--- src/de/gurkenlabs/litiengine/gui/HorizontalSlider.java
+++ src/de/gurkenlabs/litiengine/gui/HorizontalSlider.java
@@ -46,10 +46,7 @@ public class HorizontalSlider extends Slider {
             this.getButtonSpritesheet(),
             ARROW_LEFT.getText(),
             null));
-    final ImageComponent button1 = this.getButton1();
-    if (button1 != null) {
-      button1.setFont(ARROW_LEFT.getFont());
-    }
+    this.getButton1().setFont(ARROW_LEFT.getFont());
     this.setButton2(
         new ImageComponent(
             this.getX() + this.getWidth() - this.getHeight(),
@@ -59,10 +56,7 @@ public class HorizontalSlider extends Slider {
             this.getButtonSpritesheet(),
             ARROW_RIGHT.getText(),
             null));
-    final ImageComponent button2 = this.getButton2();
-    if (button2 != null) {
-      button2.setFont(ARROW_RIGHT.getFont());
-    }
+    this.getButton2().setFont(ARROW_RIGHT.getFont());
 
     final double sliderWidth = this.getHeight() * 2;
     this.minSliderX = this.getX() + this.getHeight();
