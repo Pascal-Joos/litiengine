28218a3dab61e204e170a9215fa1639230f3cb8b
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 11:49:57 +0000

    fix: 85 - RETURN_NULLABLE - return this.spritesheet; - returning @Nullable expression from method with @NonNull return type

--- src/de/gurkenlabs/litiengine/graphics/animation/AnimationController.java
+++ src/de/gurkenlabs/litiengine/graphics/animation/AnimationController.java
@@ -93,6 +93,10 @@ public class AnimationController implements IAnimationController {
   }
 
   public static Animation flipAnimation(Animation anim, String newSpriteName) {
+    if (anim == null || anim.getSpritesheet() == null) {
+      return null;
+    }
+
     final BufferedImage flippedImage = Imaging.flipSpritesHorizontally(anim.getSpritesheet());
     Spritesheet flippedSpritesheet =
         Resources.spritesheets()
