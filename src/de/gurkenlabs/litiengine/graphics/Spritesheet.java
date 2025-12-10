a95e0e92dbbad75c4944780962347abf6d768caa
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 11:46:43 +0000

    fix: 82 - RETURN_NULLABLE - return this.image; - returning @Nullable expression from method with @NonNull return type

--- src/de/gurkenlabs/litiengine/graphics/Spritesheet.java
+++ src/de/gurkenlabs/litiengine/graphics/Spritesheet.java
@@ -6,6 +6,7 @@ import de.gurkenlabs.litiengine.resources.Resources;
 import de.gurkenlabs.litiengine.util.AlphanumComparator;
 import de.gurkenlabs.litiengine.util.Imaging;
 import de.gurkenlabs.litiengine.util.io.FileUtilities;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.awt.image.RasterFormatException;
@@ -104,6 +105,7 @@ public final class Spritesheet implements Comparable<Spritesheet> {
     return scaled;
   }
 
+  @Nullable
   public BufferedImage getImage() {
     return this.image;
   }
@@ -226,10 +228,14 @@ public final class Spritesheet implements Comparable<Spritesheet> {
   }
 
   private void checkWidth(int value) {
-    checkDimension(value, this.getImage().getWidth(), this.getName(), "width");
+    checkDimension(
+        value, Nullability.castToNonnull(this.getImage()).getWidth(), this.getName(), "width");
   }
 
   private void checkHeight(int value) {
+    if (this.getImage() == null) {
+      return;
+    }
     checkDimension(value, this.getImage().getHeight(), this.getName(), "height");
   }
 
@@ -285,7 +291,7 @@ public final class Spritesheet implements Comparable<Spritesheet> {
 
   private void updateRowsAndCols() {
     final BufferedImage sprite = this.getImage();
-    this.columns = sprite.getWidth() / this.spriteWidth;
+    this.columns = Nullability.castToNonnull(sprite).getWidth() / this.spriteWidth;
     this.rows = sprite.getHeight() / this.spriteHeight;
   }
 }
