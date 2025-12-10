b986b046710f07704224341de8e4fed148389f5b
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 11:13:40 +0000

    fix: 50 - RETURN_NULLABLE - return this.parentMap; - returning @Nullable expression from method with @NonNull return type

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/ImageLayer.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/ImageLayer.java
@@ -2,6 +2,7 @@ package de.gurkenlabs.litiengine.environment.tilemap.xml;
 
 import de.gurkenlabs.litiengine.environment.tilemap.IImageLayer;
 import de.gurkenlabs.litiengine.environment.tilemap.IMapImage;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.awt.Color;
 import java.net.URL;
 import javax.annotation.Nullable;
@@ -33,7 +34,8 @@ public class ImageLayer extends Layer implements IImageLayer {
   public int getOffsetX() {
     if (this.isInfiniteMap()) {
       TmxMap map = (TmxMap) this.getMap();
-      return super.getOffsetX() - map.getChunkOffsetX() * map.getTileWidth();
+      return Nullability.castToNonnull(
+          super.getOffsetX() - map.getChunkOffsetX() * map.getTileWidth());
     }
 
     return super.getOffsetX();
@@ -43,7 +45,8 @@ public class ImageLayer extends Layer implements IImageLayer {
   public int getOffsetY() {
     if (this.isInfiniteMap()) {
       TmxMap map = (TmxMap) this.getMap();
-      return super.getOffsetX() - map.getChunkOffsetY() * map.getTileHeight();
+      return Nullability.castToNonnull(
+          super.getOffsetX() - map.getChunkOffsetY() * map.getTileHeight());
     }
 
     return super.getOffsetY();
--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/TileLayer.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/TileLayer.java
@@ -132,8 +132,13 @@ public class TileLayer extends Layer implements ITileLayer {
       this.tileList.add(tile);
       this.tiles[y][x] = tile;
     }
-    for (Tile tile : getData()) {
-      tile.setTilesetEntry(this.getMap().getTilesetEntry(tile.getGridId()));
+    if (this.getMap() != null) {
+      for (Tile tile : getData()) {
+        ITilesetEntry entry = this.getMap().getTilesetEntry(tile.getGridId());
+        if (entry != null) {
+          tile.setTilesetEntry(entry);
+        }
+      }
     }
   }
 }
