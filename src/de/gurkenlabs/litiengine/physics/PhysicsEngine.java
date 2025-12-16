56d7be0d14fcea5ac4f7ddf047a46f1b325ebaa6
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:29:51 +0000

    fix: 126 - DEREFERENCE_NULLABLE - this.collisionEntities.get(entity.getCollisionType()).remove(entity); - dereferenced expression this.collisionEntities.get(entity.getCollisionType()) is @Nullable

--- src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
+++ src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
@@ -91,6 +91,12 @@ public final class PhysicsEngine implements IUpdateable {
    *
    * @param entity The entity that is about to be removed.
    */
+  /**
+   * Removes the specified entity from any collision processing. Typically this method is implicitly
+   * called when an entity is removed from the current environment.
+   *
+   * @param entity The entity that is about to be removed.
+   */
   public void remove(final ICollisionEntity entity) {
     if (entity.getCollisionType() == null) {
       return;
@@ -99,13 +105,20 @@ public final class PhysicsEngine implements IUpdateable {
     switch (entity.getCollisionType()) {
       case DYNAMIC:
       case STATIC:
-        this.collisionEntities.get(entity.getCollisionType()).remove(entity);
+        final Collection<ICollisionEntity> entitiesByType =
+            this.collisionEntities.get(entity.getCollisionType());
+        if (entitiesByType != null) {
+          entitiesByType.remove(entity);
+        }
         break;
       default:
         return;
     }
 
-    this.collisionEntities.get(Collision.ANY).remove(entity);
+    final Collection<ICollisionEntity> anyEntities = this.collisionEntities.get(Collision.ANY);
+    if (anyEntities != null) {
+      anyEntities.remove(entity);
+    }
   }
 
   /**
