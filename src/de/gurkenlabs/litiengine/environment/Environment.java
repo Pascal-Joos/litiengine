370c1219001ce8dad8e2a9f16d07d83e6d90a328
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 16:06:34 +0000

    fix: 29 - DEREFERENCE_NULLABLE - this.renderables.get(renderType).clear(); - dereferenced expression this.renderables.get(renderType) is @Nullable

--- src/de/gurkenlabs/litiengine/environment/Environment.java
+++ src/de/gurkenlabs/litiengine/environment/Environment.java
@@ -443,6 +443,7 @@ public final class Environment implements IRenderable {
   }
 
   /** Clears all loaded entities and renderable instances from this environment. */
+  /** Clears all loaded entities and renderable instances from this environment. */
   public void clear() {
     Game.physics().clear();
 
@@ -454,9 +455,15 @@ public final class Environment implements IRenderable {
     this.allEntities.clear();
 
     for (RenderType renderType : RenderType.values()) {
-      this.miscEntities.get(renderType).clear();
-      this.renderListeners.get(renderType).clear();
-      this.renderables.get(renderType).clear();
+      if (this.miscEntities.get(renderType) != null) {
+        this.miscEntities.get(renderType).clear();
+      }
+      if (this.renderListeners.get(renderType) != null) {
+        this.renderListeners.get(renderType).clear();
+      }
+      if (this.renderables.get(renderType) != null) {
+        this.renderables.get(renderType).clear();
+      }
     }
 
     dispose(this.allEntities.values());
