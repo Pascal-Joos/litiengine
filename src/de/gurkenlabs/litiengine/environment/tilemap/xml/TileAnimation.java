3dd0178404ac7625426b11df52c6c040c2abd9e5
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 16:32:16 +0000

    fix: 58 - RETURN_NULLABLE - return this.frames; - returning @Nullable expression from method with @NonNull return type

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/TileAnimation.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/TileAnimation.java
@@ -19,6 +19,9 @@ public class TileAnimation implements ITileAnimation {
 
   @Override
   public List<ITileAnimationFrame> getFrames() {
+    if (this.frames == null) {
+      this.frames = java.util.Collections.emptyList();
+    }
     return this.frames;
   }
 
