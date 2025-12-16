97d099a46b7b34c38612cea9ab41922769a1778c
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:51:20 +0000

    fix: 11 - DEREFERENCE_NULLABLE - this.getEnvironment().getEntitiesByTag().get(tag).remove(this); - dereferenced expression this.getEnvironment().getEntitiesByTag().get(tag) is @Nullable

--- src/de/gurkenlabs/litiengine/entities/Entity.java
+++ src/de/gurkenlabs/litiengine/entities/Entity.java
@@ -417,11 +417,15 @@ public abstract class Entity implements IEntity, EntityRenderListener, Tweenable
   @Override
   public void removeTag(String tag) {
     this.getTags().remove(tag);
-    if (Game.world().environment() == null) {
+    if (Game.world().environment() == null || this.getEnvironment() == null) {
       return;
     }
-    this.getEnvironment().getEntitiesByTag().get(tag).remove(this);
-    if (this.getEnvironment().getEntitiesByTag().get(tag).isEmpty()) {
+    List<Entity> taggedEntities = this.getEnvironment().getEntitiesByTag().get(tag);
+    if (taggedEntities == null) {
+      return;
+    }
+    taggedEntities.remove(this);
+    if (taggedEntities.isEmpty()) {
       this.getEnvironment().getEntitiesByTag().remove(tag);
     }
   }
