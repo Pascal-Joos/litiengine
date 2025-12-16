4c54561d5c79f609aa7725d4ace10c1dfc089b12
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:07:26 +0000

    fix: 101 - DEREFERENCE_NULLABLE - this.getListEntry(column, row) - dereferenced expression this.getListEntry(column, row) is @Nullable

--- src/de/gurkenlabs/litiengine/gui/ListField.java
+++ src/de/gurkenlabs/litiengine/gui/ListField.java
@@ -275,24 +275,30 @@ public class ListField extends GuiComponent {
             && this.getContent()[column + this.getHorizontalLowerBound()][
                     row + this.getVerticalLowerBound()]
                 != null) {
+          ListEntry listEntry = this.getListEntry(column, row);
+          if (listEntry == null) {
+            continue;
+          }
           if (this.getContent()[column + this.getHorizontalLowerBound()][
                   row + this.getVerticalLowerBound()]
               instanceof Image) {
-            this.getListEntry(column, row)
-                .setImage(
-                    (Image)
-                        this.getContent()[column + this.getHorizontalLowerBound()][
-                            row + this.getVerticalLowerBound()]);
+            listEntry.setImage(
+                (Image)
+                    this.getContent()[column + this.getHorizontalLowerBound()][
+                        row + this.getVerticalLowerBound()]);
           } else {
-            this.getListEntry(column, row)
-                .setText(
-                    this
-                        .getContent()[column + this.getHorizontalLowerBound()][
-                        row + this.getVerticalLowerBound()]
-                        .toString());
+            listEntry.setText(
+                this
+                    .getContent()[column + this.getHorizontalLowerBound()][
+                    row + this.getVerticalLowerBound()]
+                    .toString());
           }
         } else {
-          this.getListEntry(column, row).setText("");
+          ListEntry listEntry = this.getListEntry(column, row);
+          if (listEntry == null) {
+            continue;
+          }
+          listEntry.setText("");
         }
       }
     }
@@ -302,16 +308,22 @@ public class ListField extends GuiComponent {
         && this.selectionColumn < this.getHorizontalLowerBound() + this.getNumberOfShownColumns()
         && this.selectionRow >= this.getVerticalLowerBound()
         && this.selectionRow < this.getVerticalLowerBound() + this.getNumberOfShownRows()) {
+      List<ListEntry> listEntries =
+          this.getListEntry(this.selectionColumn - this.getHorizontalLowerBound());
       this.selectedComponent =
-          this.getListEntry(this.selectionColumn - this.getHorizontalLowerBound())
-              .get(this.selectionRow - this.getVerticalLowerBound());
+          listEntries == null
+              ? null
+              : listEntries.get(this.selectionRow - this.getVerticalLowerBound());
     } else if (this.isEntireRowSelected()
         && this.selectionColumn >= 0
         && this.selectionColumn < this.nbOfColumns
         && this.selectionRow >= this.getVerticalLowerBound()
         && this.selectionRow < this.getVerticalLowerBound() + this.getNumberOfShownRows()) {
+      List<ListEntry> listEntries = this.getListEntry(0);
       this.selectedComponent =
-          this.getListEntry(0).get(this.selectionRow - this.getVerticalLowerBound());
+          listEntries == null
+              ? null
+              : listEntries.get(this.selectionRow - this.getVerticalLowerBound());
     } else {
       this.selectedComponent = null;
     }
