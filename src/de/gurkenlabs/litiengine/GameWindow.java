1be6663c38b17da9011eb4f503fd1d4b436e04f9
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:46:23 +0000

    Revert "fix: 5 - RETURN_NULLABLE - return this.resolution; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 7f113d399a53bf07f6c8449df5ec0663333a150b.

--- src/de/gurkenlabs/litiengine/GameWindow.java
+++ src/de/gurkenlabs/litiengine/GameWindow.java
@@ -203,9 +203,6 @@ public final class GameWindow {
    * @return The {@code GameWindow}'s internal resolution as a {@link Dimension}.
    */
   public Dimension getResolution() {
-    if (this.resolution == null) {
-      return new Dimension();
-    }
     return this.resolution;
   }
 
