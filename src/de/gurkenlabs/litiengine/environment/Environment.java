f400157a30e950275ad6838f8e85542e340e8363
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 16:08:15 +0000

    fix: 31 - DEREFERENCE_NULLABLE - return Collections.unmodifiableCollection(this.miscEntities.get(renderType).values()); - dereferenced expression this.miscEntities.get(renderType) is @Nullable

--- src/de/gurkenlabs/litiengine/environment/Environment.java
+++ src/de/gurkenlabs/litiengine/environment/Environment.java
@@ -1100,7 +1100,12 @@ public final class Environment implements IRenderable {
    * @see ILayer#getRenderType()
    */
   public Collection<IEntity> getEntities(final RenderType renderType) {
-    return Collections.unmodifiableCollection(this.miscEntities.get(renderType).values());
+    final Map<?, IEntity> entities = this.miscEntities.get(renderType);
+    if (entities == null) {
+      return Collections.emptySet();
+    }
+
+    return Collections.unmodifiableCollection(entities.values());
   }
 
   /**
