473530e4241c7f9a5b420fab7cd8286557e8e4d0
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:21:17 +0000

    Revert "fix: 117 - DEREFERENCE_NULLABLE - this.collisionEntities.get(Collision.ANY).add(entity); - dereferenced expression this.collisionEntities.get(Collision.ANY) is @Nullable"
    This reverts commit 5b173523103efd0a9dc2f3bd5d9622ab922ea37f.

--- src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
+++ src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
@@ -82,13 +82,7 @@ public final class PhysicsEngine implements IUpdateable {
         return;
     }
 
-    final Collection<ICollisionEntity> anyCollisionEntities =
-        this.collisionEntities.get(Collision.ANY);
-    if (anyCollisionEntities == null) {
-      return;
-    }
-
-    anyCollisionEntities.add(entity);
+    this.collisionEntities.get(Collision.ANY).add(entity);
   }
 
   /**
