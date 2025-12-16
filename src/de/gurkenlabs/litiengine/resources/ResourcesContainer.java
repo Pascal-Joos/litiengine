2b6e74d1f996c66770e50a6264f4a209e631c5ed
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:36:23 +0000

    Revert "fix: 134 - DEREFERENCE_NULLABLE - this.add(resourceName.toString(), resource); - dereferenced expression resourceName is @Nullable"
    This reverts commit 892ac1d94052bf8cd4d39b964f340f4c48beaa46.

--- src/de/gurkenlabs/litiengine/resources/ResourcesContainer.java
+++ src/de/gurkenlabs/litiengine/resources/ResourcesContainer.java
@@ -113,9 +113,6 @@ public abstract class ResourcesContainer<T> {
   }
 
   public void add(@Nullable URL resourceName, T resource) {
-    if (resourceName == null) {
-      return;
-    }
     this.add(resourceName.toString(), resource);
   }
 
