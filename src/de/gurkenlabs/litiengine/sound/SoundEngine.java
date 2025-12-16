43fc0ac16e3f10606a81e0d5e4a00b610c60f2d9
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:40:45 +0000

    fix: 140 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type

--- src/de/gurkenlabs/litiengine/sound/SoundEngine.java
+++ src/de/gurkenlabs/litiengine/sound/SoundEngine.java
@@ -648,12 +648,12 @@ public final class SoundEngine implements IUpdateable, ILaunchable {
   private SFXPlayback playSound(
       @Nullable Sound sound, Supplier<Point2D> supplier, boolean loop, int range, float volume) {
     if (sound == null) {
-      return null;
+      throw new IllegalArgumentException("sound must not be null");
     }
 
     SFXPlayback playback = createSound(sound, supplier, loop, range, volume);
     if (playback == null) {
-      return null;
+      throw new IllegalStateException("failed to create sound playback");
     }
     playback.start();
     return playback;
