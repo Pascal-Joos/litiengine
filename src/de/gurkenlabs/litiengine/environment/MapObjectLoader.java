b7a91b0b0a25a655489f0c9d306866abbaa357ce
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 16:14:18 +0000

    Revert "fix: 38 - DEREFERENCE_NULLABLE - if (!mapObject.getType().equalsIgnoreCase(this.getMapObjectType())) { - dereferenced expression mapObject.getType() is @Nullable"
    This reverts commit db5996587a7299d9a172ab41e86e1da362529be7.

--- src/de/gurkenlabs/litiengine/environment/MapObjectLoader.java
+++ src/de/gurkenlabs/litiengine/environment/MapObjectLoader.java
@@ -98,12 +98,11 @@ public abstract class MapObjectLoader implements IMapObjectLoader {
   }
 
   protected boolean isMatchingType(IMapObject mapObject) {
-    String type = mapObject.getType();
-    if (type == null || !type.equalsIgnoreCase(this.getMapObjectType())) {
+    if (!mapObject.getType().equalsIgnoreCase(this.getMapObjectType())) {
       log.log(
           Level.SEVERE,
           "Cannot load a mapobject of the type [{0}] with a loader of type [{1}].",
-          new Object[] {type, this.getClass()});
+          new Object[] {mapObject.getType(), this.getClass()});
       return false;
     }
 
