90109bb6d208ad376085c7e1caf3ec3b230e95c2
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 16:05:01 +0000

    Revert "fix: 27 - DEREFERENCE_NULLABLE - this.miscEntities.get(renderType).clear(); - dereferenced expression this.miscEntities.get(renderType) is @Nullable"
    This reverts commit f8debeeced593c48d11585424d3032cc279f20cc.

--- src/de/gurkenlabs/litiengine/environment/Environment.java
+++ src/de/gurkenlabs/litiengine/environment/Environment.java
@@ -443,7 +443,6 @@ public final class Environment implements IRenderable {
   }
 
   /** Clears all loaded entities and renderable instances from this environment. */
-  /** Clears all loaded entities and renderable instances from this environment. */
   public void clear() {
     Game.physics().clear();
 
@@ -455,18 +454,9 @@ public final class Environment implements IRenderable {
     this.allEntities.clear();
 
     for (RenderType renderType : RenderType.values()) {
-      Map<Integer, IEntity> miscMap = this.miscEntities.get(renderType);
-      if (miscMap != null) {
-        miscMap.clear();
-      }
-      Collection<IRenderListener> listeners = this.renderListeners.get(renderType);
-      if (listeners != null) {
-        listeners.clear();
-      }
-      Collection<IRenderable> renderableCollection = this.renderables.get(renderType);
-      if (renderableCollection != null) {
-        renderableCollection.clear();
-      }
+      this.miscEntities.get(renderType).clear();
+      this.renderListeners.get(renderType).clear();
+      this.renderables.get(renderType).clear();
     }
 
     dispose(this.allEntities.values());
