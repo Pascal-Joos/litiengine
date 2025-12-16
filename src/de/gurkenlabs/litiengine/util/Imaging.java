3acf9785d39b2a9bfbf512390b09228b5ff36b53
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:45:59 +0000

    Revert "fix: 146 - DEREFERENCE_NULLABLE - final Graphics2D g = dimg.createGraphics(); - dereferenced expression dimg is @Nullable"
    This reverts commit e0f41a69d1b56d7f241d1c76b56301d91b4caaf5.

--- src/de/gurkenlabs/litiengine/util/Imaging.java
+++ src/de/gurkenlabs/litiengine/util/Imaging.java
@@ -365,12 +365,6 @@ public final class Imaging {
    * @param img The image to be flipped.
    * @return The flipped image.
    */
-  /**
-   * Flips the specified image horizontally.
-   *
-   * @param img The image to be flipped.
-   * @return The flipped image.
-   */
   public static BufferedImage horizontalFlip(final BufferedImage img) {
     final int w = img.getWidth();
     final int h = img.getHeight();
@@ -379,9 +373,6 @@ public final class Imaging {
     }
 
     final BufferedImage dimg = getCompatibleImage(w, h);
-    if (dimg == null) {
-      return img;
-    }
     final Graphics2D g = dimg.createGraphics();
     g.drawImage(img, 0, 0, w, h, w, 0, 0, h, null);
     g.dispose();
