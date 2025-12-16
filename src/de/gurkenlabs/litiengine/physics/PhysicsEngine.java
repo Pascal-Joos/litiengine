60228fd60f3259b319e9f67c493f43c3998d3276
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:25:50 +0000

    fix: 122 - DEREFERENCE_NULLABLE - for (ICollisionEntity involved : involvedEntities) { - enhanced-for expression involvedEntities is @Nullable

--- src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
+++ src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
@@ -902,6 +902,10 @@ public final class PhysicsEngine implements IUpdateable {
       involvedEntities = ArrayUtilities.distinct(involvedEntities, inter.involvedEntities);
     }
 
+    if (involvedEntities == null) {
+      return;
+    }
+
     // 1. fire collision event on the collider with all the involved entities
     CollisionEvent event = new CollisionEvent(collider, involvedEntities);
     collider.fireCollisionEvent(event);
