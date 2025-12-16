41f3ee213c50ef56b3e0254d35c3caa5f6299c97
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:51:21 +0000

    Revert "fix: 11 - DEREFERENCE_NULLABLE - this.getEnvironment().getEntitiesByTag().get(tag).remove(this); - dereferenced expression this.getEnvironment().getEntitiesByTag().get(tag) is @Nullable"
    This reverts commit 97d099a46b7b34c38612cea9ab41922769a1778c.

--- src/de/gurkenlabs/litiengine/entities/Entity.java
+++ src/de/gurkenlabs/litiengine/entities/Entity.java
@@ -417,15 +417,11 @@ public abstract class Entity implements IEntity, EntityRenderListener, Tweenable
   @Override
   public void removeTag(String tag) {
     this.getTags().remove(tag);
-    if (Game.world().environment() == null || this.getEnvironment() == null) {
+    if (Game.world().environment() == null) {
       return;
     }
-    List<Entity> taggedEntities = this.getEnvironment().getEntitiesByTag().get(tag);
-    if (taggedEntities == null) {
-      return;
-    }
-    taggedEntities.remove(this);
-    if (taggedEntities.isEmpty()) {
+    this.getEnvironment().getEntitiesByTag().get(tag).remove(this);
+    if (this.getEnvironment().getEntitiesByTag().get(tag).isEmpty()) {
       this.getEnvironment().getEntitiesByTag().remove(tag);
     }
   }
