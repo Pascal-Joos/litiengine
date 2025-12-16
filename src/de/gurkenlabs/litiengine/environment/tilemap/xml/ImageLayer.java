235424ea8f35db7409b3b91f50fb228b880a273c
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 16:23:45 +0000

    Revert "fix: 49 - FIELD_NO_INIT - @XmlElement private MapImage image; - @NonNull field image not initialized"
    This reverts commit 8dbbaf81d435bd1b7db3c3f9357b108be774c3a7.

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/ImageLayer.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/ImageLayer.java
@@ -11,7 +11,7 @@ import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 public class ImageLayer extends Layer implements IImageLayer {
 
-  @XmlElement @Nullable private MapImage image;
+  @XmlElement private MapImage image;
 
   @Nullable
   @XmlAttribute
