2c861ac1396254238e3928642301308d378b82c9
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 16:03:36 +0000

    fix: 25 - DEREFERENCE_NULLABLE - this.renderables.get(renderType).add(renderable); - dereferenced expression this.renderables.get(renderType) is @Nullable

--- src/de/gurkenlabs/litiengine/environment/Environment.java
+++ src/de/gurkenlabs/litiengine/environment/Environment.java
@@ -410,6 +410,14 @@ public final class Environment implements IRenderable {
    * @see RenderEngine#renderEntity(Graphics2D, IEntity)
    */
   public void add(IRenderable renderable, RenderType renderType) {
+    if (renderType == null) {
+      return;
+    }
+
+    if (!this.renderables.containsKey(renderType)) {
+      this.renderables.put(renderType, new CopyOnWriteArrayList<>());
+    }
+
     this.renderables.get(renderType).add(renderable);
   }
 
