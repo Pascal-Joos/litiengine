3ef8bf3813b8ab7963dba2a0d90a1e803102ac72
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 13:16:27 +0000

    fix: Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/vscode/nullness-benchmarks/litiengine/src/de/gurkenlabs/litiengine/graphics/animation/Animation.java:181     return this.spritesheet;

--- src/de/gurkenlabs/litiengine/graphics/animation/Animation.java
+++ src/de/gurkenlabs/litiengine/graphics/animation/Animation.java
@@ -178,6 +178,11 @@ public class Animation implements IUpdateable, ILaunchable {
       this.initKeyFrames();
     }
 
+    if (this.spritesheet == null) {
+      throw new IllegalStateException(
+          "Spritesheet is not available for animation " + this.getName());
+    }
+
     return this.spritesheet;
   }
 
