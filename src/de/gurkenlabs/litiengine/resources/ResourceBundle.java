89997817f256fabbffcfb0afb8ffc9d3cbb07fcb
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 11:06:50 +0000

    fix: 131 - DEREFERENCE_NULLABLE - x.getName().equals(sprite.getName()) && x.getImage().equals(sprite.getImage()))) { - dereferenced expression x.getName() is @Nullable

--- src/de/gurkenlabs/litiengine/resources/ResourceBundle.java
+++ src/de/gurkenlabs/litiengine/resources/ResourceBundle.java
@@ -198,10 +198,17 @@ public class ResourceBundle implements Serializable {
   void beforeMarshal(Marshaller m) {
     List<SpritesheetResource> distinctList = new ArrayList<>();
     for (SpritesheetResource sprite : this.getSpriteSheets()) {
+      if (sprite == null || sprite.getName() == null || sprite.getImage() == null) {
+        continue;
+      }
       if (distinctList.stream()
           .anyMatch(
               x ->
-                  x.getName().equals(sprite.getName()) && x.getImage().equals(sprite.getImage()))) {
+                  x != null
+                      && x.getName() != null
+                      && x.getImage() != null
+                      && x.getName().equals(sprite.getName())
+                      && x.getImage().equals(sprite.getImage()))) {
         continue;
       }
 
@@ -212,7 +219,12 @@ public class ResourceBundle implements Serializable {
 
     List<Tileset> distinctTilesets = new ArrayList<>();
     for (Tileset tileset : this.getTilesets()) {
-      if (distinctTilesets.stream().anyMatch(x -> x.getName().equals(tileset.getName()))) {
+      if (tileset == null || tileset.getName() == null) {
+        continue;
+      }
+      if (distinctTilesets.stream()
+          .anyMatch(
+              x -> x != null && x.getName() != null && x.getName().equals(tileset.getName()))) {
         continue;
       }
 
