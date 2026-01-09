e48981d5c72673809816689330e5d8411c56ff56
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 11:13:22 +0000

    Revert "fix: 138 - DEREFERENCE_NULLABLE - if (!intro.getFormat().matches(loop.getFormat())) { - dereferenced expression intro.getFormat() is @Nullable"
    This reverts commit 4dc821e288b76629fb1252534acb3af38f821bbf.

--- src/de/gurkenlabs/litiengine/sound/IntroTrack.java
+++ src/de/gurkenlabs/litiengine/sound/IntroTrack.java
@@ -50,10 +50,8 @@ public class IntroTrack implements Track {
   public IntroTrack(Sound intro, Sound loop) {
     Objects.requireNonNull(intro);
     Objects.requireNonNull(loop);
-    AudioFormat introFormat = intro.getFormat();
-    AudioFormat loopFormat = loop.getFormat();
-    if (!introFormat.matches(loopFormat)) {
-      throw new IllegalArgumentException(introFormat + " does not match " + loopFormat);
+    if (!intro.getFormat().matches(loop.getFormat())) {
+      throw new IllegalArgumentException(intro.getFormat() + " does not match " + loop.getFormat());
     }
     this.intro = intro;
     this.loop = loop;
