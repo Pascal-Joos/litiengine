5d744b0bd8a0e0a667b3ee31662307327373fea5
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 11:12:42 +0000

    fix: 137 - DEREFERENCE_NULLABLE - tileset.finish(resourceName); - dereferenced expression tileset is @Nullable

--- src/de/gurkenlabs/litiengine/resources/Tilesets.java
+++ src/de/gurkenlabs/litiengine/resources/Tilesets.java
@@ -3,6 +3,7 @@ package de.gurkenlabs.litiengine.resources;
 import de.gurkenlabs.litiengine.environment.tilemap.xml.Tileset;
 import de.gurkenlabs.litiengine.environment.tilemap.xml.TmxException;
 import de.gurkenlabs.litiengine.util.io.XmlUtilities;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -17,7 +18,10 @@ public class Tilesets extends ResourcesContainer<Tileset> {
   protected Tileset load(@Nullable URL resourceName) throws IOException, URISyntaxException {
     try {
       Tileset tileset = XmlUtilities.read(Tileset.class, resourceName);
-      tileset.finish(resourceName);
+      if (tileset == null) {
+        return null;
+      }
+      Nullability.castToNonnull(tileset).finish(resourceName);
       return tileset;
     } catch (JAXBException e) {
       throw new TmxException(e);
