57db68df265fe0306d5afda1d92e944d780ebd6a
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 11:03:30 +0000

    fix: 127 - DEREFERENCE_NULLABLE - this.collisionBoxes.get(type).clear(); - dereferenced expression this.collisionBoxes.get(type) is @Nullable

--- src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
+++ src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
@@ -8,6 +8,7 @@ import de.gurkenlabs.litiengine.entities.IMobileEntity;
 import de.gurkenlabs.litiengine.util.ArrayUtilities;
 import de.gurkenlabs.litiengine.util.MathUtilities;
 import de.gurkenlabs.litiengine.util.geom.GeometricUtilities;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.awt.Shape;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
@@ -688,9 +689,8 @@ public final class PhysicsEngine implements IUpdateable {
         continue;
       }
 
-      this.collisionBoxes.get(type).clear();
-      this.collisionBoxes
-          .get(type)
+      Nullability.castToNonnull(this.collisionBoxes.get(type)).clear();
+      Nullability.castToNonnull(this.collisionBoxes.get(type))
           .addAll(
               this.collisionEntities.get(type).stream()
                   .map(ICollisionEntity::getCollisionBox)
