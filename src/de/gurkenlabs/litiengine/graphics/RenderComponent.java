78a04694ffccf4812fccc5f3f6eab51a9373a047
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 10:07:52 +0000

    fix: 79 - DEREFERENCE_NULLABLE - final Screen currentScreen = Game.screens().current(); - dereferenced expression Game.screens() is @Nullable

--- src/de/gurkenlabs/litiengine/graphics/RenderComponent.java
+++ src/de/gurkenlabs/litiengine/graphics/RenderComponent.java
@@ -125,7 +125,8 @@ public class RenderComponent extends Canvas {
                 ? RenderingHints.VALUE_INTERPOLATION_BILINEAR
                 : RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
 
-        final Screen currentScreen = Game.screens().current();
+        final ScreenManager screenManager = Game.screens();
+        final Screen currentScreen = screenManager != null ? screenManager.current() : null;
         if (currentScreen != null) {
           long renderStart = System.nanoTime();
           currentScreen.render(g);
