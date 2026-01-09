17214ee62a0ed8ffb5406c6bde3d385ca8e4c4c1
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 09:50:58 +0000

    Revert "fix: 60 - DEREFERENCE_NULLABLE - String[] csvTileIds = value.trim().split("[\\s]*,[\\s]*"); - dereferenced expression value is @Nullable"
    This reverts commit 069fdc193f18c8679828d6900825c344357f88c5.

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/TileData.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/TileData.java
@@ -3,7 +3,6 @@ package de.gurkenlabs.litiengine.environment.tilemap.xml;
 import com.uber.nullaway.annotations.Initializer;
 import de.gurkenlabs.litiengine.util.ArrayUtilities;
 import de.gurkenlabs.litiengine.util.io.Codec;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -346,7 +345,7 @@ public class TileData {
 
     // trim 'space', 'tab', 'newline'. pay attention to additional unicode chars
     // like \u2028, \u2029, \u0085 if necessary
-    String[] csvTileIds = Nullability.castToNonnull(value).trim().split("[\\s]*,[\\s]*");
+    String[] csvTileIds = value.trim().split("[\\s]*,[\\s]*");
 
     for (String gid : csvTileIds) {
       int tileId;
