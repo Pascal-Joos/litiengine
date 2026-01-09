753b0b6bdc367b8b98f2b3cffda801027edaf8c8
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 09:49:25 +0000

    Revert "fix: 58 - RETURN_NULLABLE - return this.frames; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 6e98588fda69f1cb0ff410911ba93461b9bbb65f.

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/TileAnimation.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/TileAnimation.java
@@ -17,7 +17,6 @@ public class TileAnimation implements ITileAnimation {
 
   private transient int totalDuration;
 
-  @Nullable
   @Override
   public List<ITileAnimationFrame> getFrames() {
     return this.frames;
@@ -29,12 +28,11 @@ public class TileAnimation implements ITileAnimation {
       return this.totalDuration;
     }
 
-    List<ITileAnimationFrame> frames = this.getFrames();
-    if (frames == null || frames.isEmpty()) {
+    if (this.getFrames().isEmpty()) {
       return 0;
     }
 
-    for (ITileAnimationFrame frame : frames) {
+    for (ITileAnimationFrame frame : this.getFrames()) {
       if (frame != null) {
         this.totalDuration += frame.getDuration();
       }
@@ -45,16 +43,8 @@ public class TileAnimation implements ITileAnimation {
 
   @Override
   public ITileAnimationFrame getCurrentFrame() {
-    long totalDuration = this.getTotalDuration();
-    if (totalDuration == 0) {
-      throw new AssertionError();
-    }
-    List<ITileAnimationFrame> frames = this.getFrames();
-    if (frames == null || frames.isEmpty()) {
-      throw new AssertionError();
-    }
-    long time = Game.time().sinceEnvironmentLoad() % totalDuration;
-    for (ITileAnimationFrame frame : frames) {
+    long time = Game.time().sinceEnvironmentLoad() % this.getTotalDuration();
+    for (ITileAnimationFrame frame : this.getFrames()) {
       time -= frame.getDuration();
       if (time <= 0) {
         return frame;
