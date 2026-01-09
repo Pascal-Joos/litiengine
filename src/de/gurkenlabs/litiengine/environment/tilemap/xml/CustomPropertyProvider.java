670bba79cd70295204728ca2daa54c6de81a0311
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 09:32:47 +0000

    fix: 47 - DEREFERENCE_NULLABLE - this.getProperty(propertyName).setType(type); - dereferenced expression this.getProperty(propertyName) is @Nullable

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/CustomPropertyProvider.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/CustomPropertyProvider.java
@@ -61,7 +61,11 @@ public class CustomPropertyProvider implements ICustomPropertyProvider {
 
   @Override
   public void setTypeOfProperty(String propertyName, String type) {
-    this.getProperty(propertyName).setType(type);
+    ICustomProperty property = this.getProperty(propertyName);
+    if (property == null) {
+      return;
+    }
+    property.setType(type);
   }
 
   @Nullable
