b139cb2dfc06c74888632eeb051f951c972cf2f2
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 11:05:57 +0000

    Revert "fix: 130 - DEREFERENCE_NULLABLE - x.getName().equals(sprite.getName()) && x.getImage().equals(sprite.getImage()))) { - dereferenced expression x.getImage() is @Nullable"
    This reverts commit d75a2a7b8baeeb1aed6776ffe24c353b218e5d2b.

--- src/de/gurkenlabs/litiengine/resources/ResourceBundle.java
+++ src/de/gurkenlabs/litiengine/resources/ResourceBundle.java
@@ -198,15 +198,10 @@ public class ResourceBundle implements Serializable {
   void beforeMarshal(Marshaller m) {
     List<SpritesheetResource> distinctList = new ArrayList<>();
     for (SpritesheetResource sprite : this.getSpriteSheets()) {
-      if (sprite.getImage() == null || sprite.getImage().isEmpty()) {
-        continue;
-      }
       if (distinctList.stream()
           .anyMatch(
               x ->
-                  x.getName().equals(sprite.getName())
-                      && x.getImage() != null
-                      && x.getImage().equals(sprite.getImage()))) {
+                  x.getName().equals(sprite.getName()) && x.getImage().equals(sprite.getImage()))) {
         continue;
       }
 
