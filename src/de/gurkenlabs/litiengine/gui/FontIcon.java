7e5890ba51000978d11920bb881b373b5ed8da34
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 14:00:08 +0000

    fix: Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/vscode/nullness-benchmarks/litiengine/src/de/gurkenlabs/litiengine/gui/FontIcon.java:39     return this.font;

--- src/de/gurkenlabs/litiengine/gui/FontIcon.java
+++ src/de/gurkenlabs/litiengine/gui/FontIcon.java
@@ -35,6 +35,7 @@ public class FontIcon {
    *
    * @return the font
    */
+  @Nullable
   public Font getFont() {
     return this.font;
   }
@@ -68,10 +69,11 @@ public class FontIcon {
     final Color oldColor = g.getColor();
     final Font oldFont = g.getFont();
     g.setColor(color);
+    final Font baseFont = this.getFont() == null ? g.getFont() : this.getFont();
     if (bold) {
-      g.setFont(this.getFont().deriveFont(Font.BOLD, fontSize));
+      g.setFont(baseFont.deriveFont(Font.BOLD, fontSize));
     } else {
-      g.setFont(this.getFont().deriveFont(fontSize));
+      g.setFont(baseFont.deriveFont(fontSize));
     }
     TextRenderer.render(g, this.getText(), x, y);
     g.setColor(oldColor);
