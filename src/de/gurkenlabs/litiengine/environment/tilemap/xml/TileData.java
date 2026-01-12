3ca51a7ea912a053d5959488a706d234423905a6
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 13:50:32 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression value is @Nullable' /home/vscode/nullness-benchmarks/litiengine/src/de/gurkenlabs/litiengine/environment/tilemap/xml/TileData.java:348     String[] csvTileIds = value.trim().split("[\\s]*,[\\s]*");

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/TileData.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/TileData.java
@@ -346,7 +346,7 @@ public class TileData {
 
     // trim 'space', 'tab', 'newline'. pay attention to additional unicode chars
     // like \u2028, \u2029, \u0085 if necessary
-    String[] csvTileIds = value.trim().split("[\\s]*,[\\s]*");
+    String[] csvTileIds = Nullability.castToNonnull(value).trim().split("[\\s]*,[\\s]*");
 
     for (String gid : csvTileIds) {
       int tileId;
