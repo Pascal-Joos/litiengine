c683caf29a6d7517600133ec5d48f0ae5def38c7
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 13:58:17 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression this.getCurrentKeyFrame() is @Nullable' /home/vscode/nullness-benchmarks/litiengine/src/de/gurkenlabs/litiengine/graphics/animation/Animation.java:319         || Game.time().since(this.lastFrameUpdate) < this.getCurrentKeyFrame().getDuration()) {

--- src/de/gurkenlabs/litiengine/graphics/animation/Animation.java
+++ src/de/gurkenlabs/litiengine/graphics/animation/Animation.java
@@ -321,9 +321,12 @@ public class Animation implements IUpdateable, ILaunchable {
 
   @Override
   public void update() {
+    if (!this.isPlaying() || this.getCurrentKeyFrame() == null) {
+      return;
+    }
+
     // do nothing if the animation is not playing or the current keyframe is not finished
-    if (!this.isPlaying()
-        || Game.time().since(this.lastFrameUpdate) < this.getCurrentKeyFrame().getDuration()) {
+    if (Game.time().since(this.lastFrameUpdate) < this.getCurrentKeyFrame().getDuration()) {
       return;
     }
 
