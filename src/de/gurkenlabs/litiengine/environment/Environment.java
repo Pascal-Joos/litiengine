40d00f7086bc1b227f21d81b94dafe9f33acadfa
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 10:58:33 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression this.renderables.get(renderType) is @Nullable' /home/vscode/nullness-benchmarks/litiengine/src/de/gurkenlabs/litiengine/environment/Environment.java:459       this.renderables.get(renderType).clear();

--- src/de/gurkenlabs/litiengine/environment/Environment.java
+++ src/de/gurkenlabs/litiengine/environment/Environment.java
@@ -458,7 +458,7 @@ public final class Environment implements IRenderable {
     for (RenderType renderType : RenderType.values()) {
       Nullability.castToNonnull(this.miscEntities.get(renderType)).clear();
       Nullability.castToNonnull(this.renderListeners.get(renderType)).clear();
-      this.renderables.get(renderType).clear();
+      Nullability.castToNonnull(this.renderables.get(renderType)).clear();
     }
 
     dispose(this.allEntities.values());
